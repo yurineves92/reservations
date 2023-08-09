c31ad15de65c0876b8951e3944be2af9bd25014d
Yuri Neves <yurineves92@gmail.com>
2023-08-09 00:37:36 -0300

    update

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use App\Models\Company;
+use App\Policies\CompanyUserPolicy;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
--- tests/Feature/CompanyUserTest.php
+++ tests/Feature/CompanyUserTest.php
@@ -2,14 +2,15 @@
 
 namespace Tests\Feature;
 
-use App\Models\Company;
 use App\Models\User;
+use App\Models\Company;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
+ 
 class CompanyUserTest extends TestCase
 {
+    use RefreshDatabase;
+ 
     public function test_admin_can_access_company_users_page()
     {
         $company = Company::factory()->create();
@@ -67,119 +68,5 @@ class CompanyUserTest extends TestCase
         $response->assertRedirect(route('companies.users.index', $company->id));
  
         $this->assertSoftDeleted($user);
-    }
-
-    public function test_company_owner_can_view_his_companies_users()
-    {
-        $company = Company::factory()->create();
-        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
-        $secondUser = User::factory()->companyOwner()->create(['company_id' => $company->id]);
- 
-        $response = $this->actingAs($user)->get(route('companies.users.index', $company->id));
- 
-        $response->assertOk()
-            ->assertSeeText($secondUser->name);
-    }
- 
-    public function test_company_owner_cannot_view_other_companies_users()
-    {
-        $company = Company::factory()->create();
-        $company2 = Company::factory()->create();
-        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
- 
-        $response = $this->actingAs($user)->get(route('companies.users.index', $company2->id));
- 
-        $response->assertForbidden();
-    }
- 
-    public function test_company_owner_can_create_user_to_his_company()
-    {
-        $company = Company::factory()->create();
-        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
- 
-        $response = $this->actingAs($user)->post(route('companies.users.store', $company->id), [
-            'name' => 'test user',
-            'email' => 'test@test.com',
-            'password' => 'password',
-        ]);
- 
-        $response->assertRedirect(route('companies.users.index', $company->id));
- 
-        $this->assertDatabaseHas('users', [
-            'name' => 'test user',
-            'email' => 'test@test.com',
-            'company_id' => $company->id,
-        ]);
-    }
- 
-    public function test_company_owner_cannot_create_user_to_other_company()
-    {
-        $company = Company::factory()->create();
-        $company2 = Company::factory()->create();
-        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
- 
-        $response = $this->actingAs($user)->post(route('companies.users.store', $company2->id), [
-            'name' => 'test user',
-            'email' => 'test@test.com',
-            'password' => 'password',
-        ]);
- 
-        $response->assertForbidden();
-    }
- 
-    public function test_company_owner_can_edit_user_for_his_company()
-    {
-        $company = Company::factory()->create();
-        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
- 
-        $response = $this->actingAs($user)->put(route('companies.users.update', [$company->id, $user->id]), [
-            'name' => 'updated user',
-            'email' => 'test@update.com',
-        ]);
- 
-        $response->assertRedirect(route('companies.users.index', $company->id));
- 
-        $this->assertDatabaseHas('users', [
-            'name' => 'updated user',
-            'email' => 'test@update.com',
-            'company_id' => $company->id,
-        ]);
-    }
- 
-    public function test_company_owner_cannot_edit_user_for_other_company()
-    {
-        $company = Company::factory()->create();
-        $company2 = Company::factory()->create();
-        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
- 
-        $response = $this->actingAs($user)->put(route('companies.users.update', [$company2->id, $user->id]), [
-            'name' => 'updated user',
-            'email' => 'test@update.com',
-        ]);
- 
-        $response->assertForbidden();
-    }
- 
-    public function test_company_owner_can_delete_user_for_his_company()
-    {
-        $company = Company::factory()->create();
-        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
- 
-        $response = $this->actingAs($user)->delete(route('companies.users.destroy', [$company->id, $user->id]));
- 
-        $response->assertRedirect(route('companies.users.index', $company->id));
- 
-        $this->assertSoftDeleted($user);
-    }
- 
-    public function test_company_owner_cannot_delete_user_for_other_company()
-    {
-        $company = Company::factory()->create();
-        $company2 = Company::factory()->create();
-        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
- 
-        $response = $this->actingAs($user)->delete(route('companies.users.destroy', [$company2->id, $user->id]));
- 
-        $response->assertForbidden();
     }
 }
